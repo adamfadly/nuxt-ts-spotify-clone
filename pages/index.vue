2a5cc8a0c56b413c45040e44216cc560f40b6fe1
Adam Fadly <adamfadly11@gmail.com>
2021-10-16 15:21:41 +0700

    cleaning up

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      name: 'Adam Fadly',
+    }
+  },
+})
+</script>
