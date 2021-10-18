b4082139dd6d8df6237669845924c2c70b930d11
Adam Fadly <adamfadly11@gmail.com>
2021-10-18 08:19:40 +0700

    adding new router and glbal css for body background color and text color

--- components/Navbar/LeftNavbar.vue
+++ components/Navbar/LeftNavbar.vue
@@ -1,60 +1,56 @@
 vbase
 <template>
-  <div class="flex flex-col w-1/6 bg-red-900 h-screen">
+  <div class="flex flex-col w-1/6 bg-black h-screen">
     <img
       class="w-full h-22 py-3 px-6"
       src="../../assets/logo/Spotify_Logo_White.png"
       alt=""
     />
     <div class="flex flex-col py-1 px-6">
-      <div class="flex items-center">
-        <HomeIcon class="text-white w-6 h-6" />
-        <p class="text-white text-lg font-normal py-2 pl-3">Home</p>
-      </div>
-      <div class="flex items-center">
-        <LibraryIcon class="text-white w-6 h-6" />
-        <p class="text-white text-lg font-normal py-2 pl-3">Library</p>
-      </div>
-      <div class="flex items-center">
-        <SearchIcon class="text-white w-6 h-6" />
-        <p class="text-white text-lg font-normal py-2 pl-3">Search</p>
-      </div>
+      <nuxt-link to="/home">
+        <div class="flex items-center">
+          <HomeIcon class="w-6 h-6" />
+          <p class="text-lg font-normal py-2 pl-3">Home</p>
+        </div>
+      </nuxt-link>
+      <nuxt-link to="/library">
+        <div class="flex items-center">
+          <LibraryIcon class="w-6 h-6" />
+          <p class="text-lg font-normal py-2 pl-3">Library</p>
+        </div>
+      </nuxt-link>
+      <nuxt-link to="/search">
+        <div class="flex items-center">
+          <SearchIcon class="w-6 h-6" />
+          <p class="text-lg font-normal py-2 pl-3">Search</p>
+        </div>
+      </nuxt-link>
     </div>
     <div class="flex flex-col pt-4 pb-1 px-6">
       <div class="flex items-center">
         <PlusIcon class="text-black text-xs bg-white w-6 h-6 p-1" />
-        <p class="text-white text-lg font-normal py-2 pl-3">Create Playlist</p>
+        <p class="text-lg font-normal py-2 pl-3">Create Playlist</p>
       </div>
       <div class="flex items-center">
         <LoveIcon
-          class="
-            text-white text-xs
-            p-1
-            w-6
-            h-6
-            bg-gradient-to-r
-            from-blue-700
-            to-blue-300
-          "
+          class="text-xs p-1 w-6 h-6 bg-gradient-to-r from-blue-700 to-blue-300"
         />
-        <p class="text-white text-lg font-normal py-2 pl-3">Liked Songs</p>
+        <p class="text-lg font-normal py-2 pl-3">Liked Songs</p>
       </div>
       <div class="flex items-center">
         <PodcastIcon
           class="text-green w-6 h-6 bg-green bg-opacity-50 rounded-sm p-1"
         />
-        <p class="text-white text-lg font-normal py-2 pl-3">Your Episode</p>
+        <p class="text-lg font-normal py-2 pl-3">Your Episode</p>
       </div>
     </div>
     <hr class="bg-gray-300 mx-6" />
     <div class="py-1 overflow-y-auto px-6">
-      <p v-for="n in 100" :key="n" class="text-sm text-white py-1">
-        My Playlist
-      </p>
+      <p v-for="n in 50" :key="n" class="text-sm py-1">My Playlist</p>
     </div>
     <div class="flex items-center px-6 py-5 h-10 pb-10">
-      <ArrowDownIcon class="text-white w-6 h-6" />
-      <p class="text-white text-lg font-normal py-2 pl-3">Install App</p>
+      <ArrowDownIcon class="w-6 h-6" />
+      <p class="text-lg font-normal py-2 pl-3">Install App</p>
     </div>
   </div>
 </template>
