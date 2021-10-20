4d03070c3b8ec2d1ea8e9ae309f47e5509f43b58
Adam Fadly <adamfadly11@gmail.com>
2021-10-20 10:36:09 +0700

    layout done

--- layout/default.vue
+++ layout/default.vue
@@ -1,17 +1,5 @@
 <template>
-  <div class="bg-black h-screen w-full flex">
-    <left-navbar></left-navbar>
+  <div>
     <nuxt />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-import LeftNavbar from '~/components/Navbar/LeftNavbar.vue'
-
-export default Vue.extend({
-  components: { LeftNavbar },
-})
-</script>
-
-<style scoped></style>
