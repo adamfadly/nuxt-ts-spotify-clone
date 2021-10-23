ee2306901161766c9822fded903f6e94ee143b15
Adam Fadly <adamfadly11@gmail.com>
2021-10-23 09:42:26 +0700

    tab playlist done

--- pages/Home/index.vue
+++ pages/Home/index.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="bg-gray w-full h-screen">
+  <div class="bg-black container">
     <top-navbar></top-navbar>
+    <div class="px-5">
+      <p class="text-4xl font-bold py-5">Good Morning</p>
+      <div class="flex flex-wrap justify-between">
+        <div v-for="n in 6" :key="n">
+          <play-list-tab></play-list-tab>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import TopNavbar from '~/components/Navbar/TopNavbar.vue'
+import PlayListTab from '~/components/Playlist/PlayListTab.vue'
 
 export default Vue.extend({
-  layout: 'mainView',
   components: {
     TopNavbar,
+    PlayListTab,
   },
+  layout: 'mainView',
 })
 </script>
 
