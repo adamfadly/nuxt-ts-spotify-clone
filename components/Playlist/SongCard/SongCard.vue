584383357c30361dad5f0f03fb99ed6b3bb11e21
Adam Fadly <adamfadly11@gmail.com>
2021-11-16 00:46:42 +0700

    done with song card

---
+++ components/Playlist/SongCard/SongCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div v-for="(song, index) in 5" :key="index" class="">
+      <div
+        class="wrap-card w-52 h-64 bg-gray flex flex-col p-4 rounded-sm"
+        :class="isHovering ? 'bg-opacity-100' : 'bg-opacity-25'"
+        @mouseover="onHover"
+        @mouseout="onHover"
+      >
+        <div class="mb-2 relative">
+          <div class="bg-black w-44 h-44 rounded-sm relative"></div>
+          <play
+            :class="isHovering ? 'opacity-100' : 'opacity-0'"
+            class="absolute bottom-2 right-2 bg-green rounded-full w-10 h-10"
+          ></play>
+        </div>
+        <div class="flex flex-col text-left">
+          <p class="text-base font-medium">Title</p>
+          <p class="font-medium text-gray-dark text-sm">artist</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Play from '~/assets/icon/HeroIcons/Play.vue'
+
+export default Vue.extend({
+  components: { Play },
+  props: {
+    category: {
+      type: String,
+      default: '',
+    },
+    items: {
+      type: Number,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      isHovering: false,
+    }
+  },
+  methods: {
+    onHover(): void {
+      console.log('yolo')
+      this.isHovering = !this.isHovering
+    },
+  },
+})
+</script>
--- pages/Home/index.vue
+++ pages/Home/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="bg-black container">
+  <div class="bg-black container h-screen">
     <top-navbar></top-navbar>
-    <div class="px-5">
-      <p class="text-4xl font-bold py-5">Good Morning</p>
-      <div class="flex flex-wrap justify-between">
-        <div v-for="n in 6" :key="n">
-          <play-list-tab></play-list-tab>
+    <div class="px-5 overflow-y-auto h-screen">
+      <!-- tab list -->
+      <div>
+        <p class="text-4xl font-bold py-5">Good Morning</p>
+        <div class="flex flex-wrap justify-between">
+          <div v-for="(song, index) in 6" :key="index">
+            <play-list-tab></play-list-tab>
+          </div>
         </div>
+      </div>
+      <!-- song card test -->
+      <div class="">
+        <p class="text-4xl font-bold py-5">Episodes for you</p>
+        <song-card class="flex justify-between" :items="5"></song-card>
       </div>
     </div>
   </div>
@@ -16,11 +24,13 @@
 import Vue from 'vue'
 import TopNavbar from '~/components/Navbar/TopNavbar.vue'
 import PlayListTab from '~/components/Playlist/PlayListTab.vue'
+import SongCard from '~/components/Playlist/SongCard/SongCard.vue'
 
 export default Vue.extend({
   components: {
     TopNavbar,
     PlayListTab,
+    SongCard,
   },
   layout: 'mainView',
 })
